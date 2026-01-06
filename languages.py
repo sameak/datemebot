e8e3704785f8df3f91f44876b9494bbcd90793e0
sameak <37511755+sameak@users.noreply.github.com>
2026-01-07 02:03:52 +0900

    Add language support for English and Khmer

--- languages.py
+++ languages.py
@@ -1,1 +1,10 @@
-
+TEXT = {
+    "en": {
+        "welcome": "💖 Welcome to DateMe\nChoose language",
+        "age_check": "🔞 Are you 18 or older?"
+    },
+    "kh": {
+        "welcome": "💖 ស្វាគមន៍មកកាន់ DateMe\nជ្រើសរើសភាសា",
+        "age_check": "🔞 តើអ្នកមានអាយុ 18+ ទេ?"
+    }
+}
