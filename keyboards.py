add019cd16a2af43ac3eab97987b4bb1c6a63f10
sameak <37511755+sameak@users.noreply.github.com>
2026-01-07 02:04:44 +0900

    Add Telegram reply keyboard functions

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,7 @@
+from telegram import ReplyKeyboardMarkup
 
+def lang_keyboard():
+    return ReplyKeyboardMarkup([["K", "E"]], resize_keyboard=True)
+
+def yes_no():
+    return ReplyKeyboardMarkup([["Y", "N"]], resize_keyboard=True)
